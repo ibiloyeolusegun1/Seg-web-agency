dd492a6b0af1f551045548e46fd0f7f5bf9f88f8
ibiloyeolusegun1 <ibiloyeolusegun.1@gmail.com>
2023-09-18 00:03:52 +0100

    index.html, App.js and  index.css were updated

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Header from "./components/Header/Header";
 import { MdAnalytics } from "react-icons/md";
 import './index.css';
 import './App.css';
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Hero/>
     </div>
   );
 }
