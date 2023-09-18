a2385adf9caa2ea4d0f70aa929548b6090ef7a33
ibiloyeolusegun1 <ibiloyeolusegun.1@gmail.com>
2023-09-19 00:02:27 +0100

    Added about.jsx and about.css file
    Updated App.js and index.css files

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { MdAnalytics } from "react-icons/md";
 import './index.css';
 import './App.css';
 import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Hero/>
+      <About/>
     </div>
   );
 }
---
+++ src/components/About/About.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import "./About.css";
+import { BiDesktop, BiPaintRoll, BiBarChartAlt2, BiDiamond, BiDetail, BiBrush } from 'react-icons/bi';
+
+const About = () => {
+  return (
+    <section className="about">
+        <div className="about-container padding inner-w">
+            <h1>What we do</h1>
+            <div className="about-content">
+                <div className="box">
+                    <h2>APP DEVELOPMENT</h2>
+                    <p>Some quick example text to build on the card title and make up the bulk of the card content in the box</p>
+                    <a href="#">Contact us</a>
+                    <div className="icon">
+                        <BiDesktop/>
+                    </div>
+                    <div className="blue"></div>
+                </div>
+                <div className="box">
+                    <h2>WEBSITE DEVELOPMENT</h2>
+                    <p>Some quick example text to build on the card title and make up the bulk of the card content in the box</p>
+                    <a href="#">Contact us</a>
+                    <div className="icon">
+                        <BiDetail/>
+                    </div>
+                    <div className="blue"></div>
+                </div>
+                <div className="box">
+                    <h2>UI/UX DESIGNING</h2>
+                    <p>Some quick example text to build on the card title and make up the bulk of the card content in the box</p>
+                    <a href="#">Contact us</a>
+                    <div className="icon">
+                        <BiBrush/>
+                    </div>
+                    <div className="blue"></div>
+                </div>
+                <div className="box">
+                    <h2>DIGITAL MARKETING</h2>
+                    <p>Some quick example text to build on the card title and make up the bulk of the card content in the box</p>
+                    <a href="#">Contact us</a>
+                    <div className="icon">
+                        <BiDiamond/>
+                    </div>
+                    <div className="blue"></div>
+                </div>
+                <div className="box">
+                    <h2>SEO & CONTENT WRITING</h2>
+                    <p>Some quick example text to build on the card title and make up the bulk of the card content in the box</p>
+                    <a href="#">Contact us</a>
+                    <div className="icon">
+                        <BiBarChartAlt2/>
+                    </div>
+                    <div className="blue"></div>
+                </div>
+                <div className="box">
+                    <h2>GRAPHIC DESIGNING</h2>
+                    <p>Some quick example text to build on the card title and make up the bulk of the card content in the box</p>
+                    <a href="#">Contact us</a>
+                    <div className="icon">
+                        <BiPaintRoll/>
+                    </div>
+                    <div className="blue"></div>
+                </div>
+                <div className="box">
+                    <h2>SOLUTION FOR <br /> YOUR BUSINESS</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit.</p>
+                    <div className="btn">
+                        Learn more
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default About
